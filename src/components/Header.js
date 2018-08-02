0b05189e02ad7116ae465ac8a10d81b28611dca7
Raul Coral <rfcoralj@unal.edu.co>
2018-08-02 18:04:16 -0500

    Landing Animated!

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,12 +5,16 @@ import { Link } from 'react-router-dom'
 
 export default class Header extends Component {
   render() {
+    const auxMargin = { marginRight: 8 }
+    const noPadding = { padding: 0 }
     return (
       <header>
         <nav className="navbar navbar-dark bg-dark">
-          <a className="navbar-brand logo" href="/">:v UNfail</a>
+          <Link className="navbar-brand logo" style={ noPadding } to="/">
+            <span className='brand-logo-big logo'>:v</span> UNfail
+          </Link>
           <div className='justify-content-end'>
-            <Link className="btn btn-outline-light mr-sm-2"
+            <Link className="btn btn-outline-light" style={ auxMargin }
               to='/signin'>Sign In</Link>
             <Link className="btn btn-outline-light" to='/signup'>Sign Up</Link>
           </div>
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,18 +1,35 @@
 import React, { Component } from 'react'
+import $ from 'jquery'
+
+import bgLanding from '../assets/bg-landing.jpg'
 
 export default class Landing extends Component {
 
   componentDidMount() {
     document.title = 'UNfail'
   }
+
+  handleLoad(event) {
+    $('.bg-landing').css('display','block')
+    $('.paused').removeClass('paused')
+  }
+
   render() {
+    const style = { minHeight: 350, position: 'relative' }
     return (
-      <div className='bg-landing full-size'>
+      <div className='full-size' style={ style }>
+        <img src={ bgLanding } className='bg-landing' alt='bg'
+          onLoad={ (e) => this.handleLoad(e) } />
         <div className='all-center text-center'>
-          <h1 className="display-1 logo text-white animated bounceInDown">
-            UNfail :v
+          <h1 className='super-big logo text-white animated paused bounceInDown'>
+            :v
           </h1>
-          <h2 className='animated fadeIn delay-1 text-white'>Es una mini app de login :3</h2>
+          <h1 className="display-2 logo text-white delay-1 animated paused fadeIn">
+            UNfail
+          </h1>
+          <h3 className='delay-1 animated paused fadeIn text-white'>
+            Es una mini app de login :3
+          </h3>
         </div>
       </div>
     )
